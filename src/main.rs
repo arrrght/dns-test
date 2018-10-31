9b38f07f48ae09ce7f783fc7241df707fdda5b4a
arrrght <arrrght@gmail.com>
2018-10-31 18:45:43 +0500

    switch to dns-parser

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,48 @@
-extern crate dns_lookup;
-use dns_lookup::lookup_host;
+extern crate dns_parser;
 use std::net::IpAddr;
+use std::net::UdpSocket;
 use std::time::Instant;
+
+use dns_parser::rdata::a::Record;
+use dns_parser::{Builder, Packet, RData, ResponseCode};
+use dns_parser::{QueryClass, QueryType};
 
 fn main() {
     let mut arr: [u32; 10] = [0; 10];
+    let sock = UdpSocket::bind("127.0.0.1:0").unwrap();
+    sock.connect("127.0.0.1:53").unwrap();
+
     for i in 0..9 {
         let now = Instant::now();
-        let host = "google.com";
-        let ips: Vec<IpAddr> = lookup_host(&host).unwrap();
-        arr[i] = now.elapsed().subsec_micros();
-        println!("{}, time:{}", ips[0], arr[i]);
+        let host = "e1.ru";
+        let mut builder = Builder::new_query(1, true);
+        builder.add_question(host, false, QueryType::A, QueryClass::IN);
+        let packet = builder.build().map_err(|_| "truncated packet").unwrap();
+        sock.send(&packet);
+        let mut buf = vec![0u8; 4096];
+        sock.recv(&mut buf).unwrap();
+        let pkt = Packet::parse(&buf).unwrap();
+
+        if pkt.header.response_code != ResponseCode::NoError {
+            assert!(true, false); //TODO
+        }
+        if pkt.answers.len() == 0 {
+            assert!(true, false); //TODO
+        }
+        for ans in pkt.answers {
+            match ans.data {
+                RData::A(Record(ip)) => {
+                    arr[i] = now.elapsed().subsec_micros();
+                    println!("found: {}, time:{}", ip, arr[i]);
+                }
+                _ => {}
+            }
+        }
+
     }
     arr.sort();
     let sum: u32 = arr.iter().sum();
     let aver: f32 = sum as f32 / arr.len() as f32;
     let median = arr[arr.len() / 2];
-    
     println!("average: {}, median: {}", aver, median);
 }
